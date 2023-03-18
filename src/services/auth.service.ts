fefbe70faad59fbf546e7eee7efd002b778fa848
Alexis Leloup <alexis.leloup@epitech.eu>
2023-03-18 22:44:24 +0100

    Fixing implementation of asynchronous function createAccessToken

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,20 +3,16 @@ import axiosInstance from '../config/axios.instance';
 import { setAccessToken, getAccessToken } from '../utils/credentials';
 
 export class DeezerAuth {
-    public createAccessToken = (appId: string, secretId: string, code: string) => {
+    public async createAccessToken(appId: string, secretId: string, code: string) {
         const query: string = `app_id=${appId}&secret=${secretId}&code=${code}`;
 
-        return axiosInstance
-            .get(`oauth/access_token.php?${query}`, { baseURL: 'https://connect.deezer.com/' })
-            .then((response: AxiosResponse) => {
-                const accessToken: string = response.data.split('&')[0].split('=')[1];
-                setAccessToken(accessToken);
-                return getAccessToken();
-            })
-            .catch((error: any) => {
-                return error;
-            });
-    };
+        const response = await axiosInstance.get(`oauth/access_token.php?${query}`, {
+            baseURL: 'https://connect.deezer.com/',
+        });
+        const accessToken: string = response.data.split('&')[0].split('=')[1];
+        setAccessToken(accessToken);
+        return getAccessToken();
+    }
 
     public generateAuthenticationLink = (appId: string, redirectUri: string, perm?: string) => {
         const query: string = `app_id=${appId}&redirect_uri=${redirectUri}&perms=${perm}`;
